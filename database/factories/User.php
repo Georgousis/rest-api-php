eeeb74539294d7ab7b0de52e9de82ee2862c8e41
Georgousis Giannis <georgousis@thesmilinghippo.com>
2024-12-17 13:26:24 +0200

    model factores scafolding

--- database/factories/User.php
+++ database/factories/User.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Str;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
+class User extends Factory
 {
     /**
      * The current password being used by the factory.
@@ -24,11 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'first_name' => fake()->name(),
+            'last_name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail()
+            //'email_verified_at' => now(),
+            //'password' => static::$password ??= Hash::make('password'),
+            //'remember_token' => Str::random(10),
         ];
     }
 
